e0e4ebe123073400f535891188d7c567dde8c3f7
19  5  2  1  19  20  9  1  14 <sebastianm880@deltalearns.ca>
2022-01-27 13:50:44 -0800

    Update x01_deck.py

--- x01_deck.py
+++ x01_deck.py
@@ -9,28 +9,25 @@ as well as a suit
 def createDeck():
   ranks = ['2','3','4','5','6','7','8','9','T','J','Q','K','A']
   suits = ['C','D','H','S']
-
+  a = []
   for c in ranks:
     for l in suits:
       deck = [c + l]
-      print(deck)
-      return deck
+      a = a + deck
+  return a
+#createDeck()
 
 
 
-  '''
-  use the two lists to create a new list "deck" 
-  return the deck list to your calling function
-  '''
-createDeck()
-'''
+
+
 def main():
   deck = createDeck()
   assert "JH" in deck 
+  print(deck)
   assert "AC" in deck 
   assert "TD" in deck 
   assert len(deck) == 52
   
 if __name__ == "__main__":
   main()
-'''
